7946a5ef6eac4e25d3a84873189efa6cd2f302d1
tandukuda <where949@gmail.com>
2026-01-21 13:09:27 +0700

    Revert image configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,9 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  // Static site generation
   output: "static",
   build: {
     format: "directory",
   },
-  // Image optimization
-  image: {
-    service: {
-      entrypoint: "astro/assets/services/sharp",
-    },
-  },
-  // Minimal optimizations
   compressHTML: true,
-  site: "https://monograph.vercel.app", // Update this with your domain
 });
